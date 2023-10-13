eac9ad154465ab9d4b235f8b634bc2b65332b306
alsaritnj <alsaritnj@gmail.com>
2023-10-13 15:17:41 +0300

    Separete tree search to other file

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 movement = require("robomvmt")
+require("treeSearch")
 
 treeGrowingWaitingTime = 10;
 geolyserMaxRange = 5 -- default 32
@@ -29,68 +30,7 @@ function fillDistMatrix()
     end
 end
 
-function getTrees()
-    local trees = {}
-    trees[-7] = {}
-    trees[-7][1] = 1
-    trees[-7][6] = 1
-    trees[-7][14] = 1
 
-    trees[0] = {}
-    trees[0][-1] = 1
-    trees[0][3] = 1
-    trees[0][10] = 1
-    --trees[3] = {}
-    --trees[6] = {}
-    return trees
-end
-
-function getTreesInArea()
-    local trees = getTrees()
-    local treesInArea = {}
-
-    -- calculating either trees zone xor scanned zone is closer. then converting it to relative coord
-    xMin = math.max(movement.x - geolyserMaxRange, 0) - area.widthCenter
-    zMin = math.max(movement.z - geolyserMaxRange, 0) - area.heightCenter
-
-    xMax = math.min(movement.x + geolyserMaxRange, area.width) - area.widthCenter
-    zMax = math.min(movement.z + geolyserMaxRange, area.height) - area.heightCenter
-
-    for x, rows in pairs(trees) do
-        treesInArea[x] = {}
-        for z, _ in pairs(rows) do
-            if (x >= xMin and x <= xMax) and (z >= zMin and z <= zMax) then
-                treesInArea[x][z] = 1
-            end
-        end
-    end
-
-    return treesInArea
-end
-
-function getNearestTree()
-    local trees = getTreesInArea()
-
-    nearestTree = {}
-    for x, rows in pairs(trees) do
-        for z, _ in pairs(rows) do
-            if (not nearestTree.x or not nearestTree.z) 
-                or (distMatrix[x][z] < distMatrix[nearestTree.x][nearestTree.z]) then
-                nearestTree.x = x
-                nearestTree.z = z
-            end
-        end
-    end
-
-    
-    if not nearestTree.x and not nearestTree.z then
-        -- transfer to global coords
-        nearestTree.x = nearestTree.x + movement.x
-        nearestTree.z = nearestTree.z + movement.z
-
-        return nearestTree
-    end 
-end
 
 function main()
     startUp(robotCords, robotDir, areaWidth, areaHeight)
