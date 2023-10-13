d87452719eb207dd81d1a779e8efefa4209b04ef
alsaritnj <alsaritnj@gmail.com>
2023-10-13 16:11:26 +0300

    fix transformToRelative() changed name changed "ath" to "math"

--- coordsTransform.lua
+++ coordsTransform.lua
@@ -1,4 +1,4 @@
 -- translate coords to robot relative
-function tranformToRelative(x, z)
-    return {math.abs(x) - math.abs(movement.x), ath.abs(movement.y) - math.abs(robotY)}
+function transformToRelative(x, z)
+    return {math.abs(x) - math.abs(movement.x), math.abs(movement.y) - math.abs(robotY)}
 end
