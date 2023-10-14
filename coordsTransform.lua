4eeb7f7e35abebe54a27bd279ca21aa54973710e
alsaritnj <alsaritnj@gmail.com>
2023-10-14 13:26:42 +0300

    Renate "relativeToRovot" to "robotRelative"

--- coordsTransform.lua
+++ coordsTransform.lua
@@ -1,7 +1,7 @@
 local coordsTransform = {}
 
 -- translate coords to robot relative
-function coordsTransform.relativeToRobot(coords)
+function coordsTransform.robotRelative(coords)
     return {math.abs(coords.x) - math.abs(movement.x), math.abs(coords.y) - math.abs(movement.y)}
 end
 
