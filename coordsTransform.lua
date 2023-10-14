1719f8fbb2ee8b480b7f723c7bbd683ec9f801f8
alsaritnj <alsaritnj@gmail.com>
2023-10-14 16:56:55 +0300

    Add coordsTransform.robotGlobal()

--- coordsTransform.lua
+++ coordsTransform.lua
@@ -11,6 +11,14 @@ function coordsTransform.robotRelative(coords, position)
     )
 end
 
+-- transform coords from robot relative to global
+function coordsTransform.robotGlobal(coords, position)
+    return vectors.new2d(
+        coords.x + position.x, 
+        coords.y + position.y
+    )
+end
+
 --[[ converts from global to geolyzer relative
 while global coords begins from 0;0, geolyzer's begins from -geolyzerRange]]--
 function coordsTransform.geolyzerRelative(globalCoords)
