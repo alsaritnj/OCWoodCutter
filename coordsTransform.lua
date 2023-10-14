a60a867824ec2eaa9f0c46427cddc95b2087c62c
alsaritnj <alsaritnj@gmail.com>
2023-10-14 13:34:30 +0300

    Add geolyzerRelative() function It converts global coords to geolyze's

--- coordsTransform.lua
+++ coordsTransform.lua
@@ -1,4 +1,5 @@
 local vectors = require("lib/vectors")
+local settings = require("settings")
 
 local coordsTransform = {}
 
@@ -7,6 +8,15 @@ function coordsTransform.robotRelative(coords)
     return vectors.new2d(
         math.abs(coords.x) - math.abs(movement.x), 
         math.abs(coords.y) - math.abs(movement.y)
+    )
+end
+
+--[[ converts from global to geolyzer relative
+while global coords begins from 0;0, geolyzer's begins from -geolyzerRange]]--
+function coordsTransform.geolyzerRelative(globalCoords)
+    return vectors.new2d(
+        globalCoords.x - settings.geolyzerRange, 
+        globalCoords.y - settings.geolyzerRange
     )
 end
 
