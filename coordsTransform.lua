365f009aca9d664b1bdd785cd5feaa266e40dc10
alsaritnj <alsaritnj@gmail.com>
2023-10-14 13:01:45 +0300

    transformCoordsToRelative() changed to coordsTransform.relative()

--- coordsTransform.lua
+++ coordsTransform.lua
@@ -1,4 +1,8 @@
+local coordsTransform = {}
+
 -- translate coords to robot relative
-function transformToRelative(x, z)
+function coordsTransform.relative(x, z)
     return {math.abs(x) - math.abs(movement.x), math.abs(y) - math.abs(movement.y)}
 end
+
+return coordsTransform
