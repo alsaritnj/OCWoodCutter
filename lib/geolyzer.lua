dd93cfed5272df9183de66bf288a5a010b0831a1
alsaritnj <alsaritnj@gmail.com>
2023-10-15 16:56:21 +0300

    Geolyzer returns the same number of blocks as in the game

--- lib/geolyzer.lua
+++ lib/geolyzer.lua
@@ -5,7 +5,7 @@ function geolyzer.scan()
     local arr = {}
     
     local str = "geolyzer: "
-    for i = -settings.geolyzerRange, settings.geolyzerRange do
+    for i = 1, 64 do
         arr[i] = tonumber(string.format("%.1f", (math.random(0, 15) / 10)))
         str = str .. arr[i] .. "\t"
     end
