f8332824adb446d9c2bd22e6ae00214b45cc6fee
alsaritnj <alsaritnj@gmail.com>
2023-10-15 13:07:30 +0300

    Add woodMinHardness to settings

--- settings.lua
+++ settings.lua
@@ -5,5 +5,6 @@ return {
     area = vectors.new2d(10, 10),
     scanInterval = 10, -- in seconds
     geolyzerRange = 5, -- default: 32
-    startPosition = vectors.new3d(5, 0, 5)
+    startPosition = vectors.new3d(5, 0, 5),
+    woodMinHardness = 1.5
 }
